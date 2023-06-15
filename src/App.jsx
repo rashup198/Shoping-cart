b9eb22700dd381efd95cb5afaccc292ff44e656e
Priyanshu Pandey <88549100+rashup198@users.noreply.github.com>
2023-06-15 23:33:54 +0530

    updated App.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,25 @@
+import React from "react";
+import { Route,Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import Cart from "./pages/Cart";
+import "./index.css";
+
+
 const App = () => {
-  return(  <div>
-    App
+  return(
+  <div>
+    hello
+   <div>
+    <Navbar></Navbar>
+   </div>
+   <Routes>
+    <Route path="/" element={<Home/>}></Route>
+    <Route path="/cart" element={<Cart/>}></Route>
+    
+
+   </Routes>
+   
   </div>
   )
 };
