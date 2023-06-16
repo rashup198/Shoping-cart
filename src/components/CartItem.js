034a1a30f8b3e078b98b19f2222e95951525f173
Priyanshu Pandey <88549100+rashup198@users.noreply.github.com>
2023-06-16 22:12:26 +0530

    updated CartItem.js

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -1,7 +1,35 @@
-const CartItem=()=>{
+import { toast } from 'react-hot-toast';
+import {FcDeleteDatabase} from 'react-icons/fc';
+import { useDispatch } from 'react-redux';
+import { remove } from '../redux/Slices/CartSlice';
+
+
+const CartItem=({item, itemIndex})=>{
+    const dispatch = useDispatch();
+
+    const removeFromCart=()=>{
+        dispatch(remove(item.id));
+        toast.success("Item Removed")
+    }
+
     return(
         <div>
-            CartItem
+        <div>
+            <img src={item.image}></img>
+        </div>
+        <div>
+            <h1>
+                {item.title}
+            </h1>
+            <h1>{item.description}</h1>
+            <div>
+                <p>{item.price}</p>
+                <div onClick={removeFromCart}>
+                    <FcDeleteDatabase></FcDeleteDatabase>
+                </div>
+            </div>
+        </div>
+            
         </div>
     )
 }
