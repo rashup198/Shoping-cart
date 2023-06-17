6a2280f92270f147c662989641625390fe6cdcad
Priyanshu Pandey <88549100+rashup198@users.noreply.github.com>
2023-06-17 21:03:47 +0530

    updated Spinner.js

--- src/components/Spinner.js
+++ src/components/Spinner.js
@@ -3,8 +3,9 @@ import "./Spinner.css"
 
 const Spinner = () => {
   return (
-    <div>
+    <div className='h-screen flex flex-col gap-y-4 justify-center items-center'>
     <div class="custom-loader"></div>
+    <h1 className='text-xl'>Loading</h1>
     </div>
   )
 }
