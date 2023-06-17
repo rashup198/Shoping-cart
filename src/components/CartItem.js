234210d20c3261f3e8c796ca98cc2e956362ed72
Priyanshu Pandey <88549100+rashup198@users.noreply.github.com>
2023-06-17 21:03:32 +0530

    updated CartItem.js

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -4,6 +4,7 @@ import { useDispatch } from 'react-redux';
 import { remove } from '../redux/Slices/CartSlice';
 
 
+
 const CartItem=({item, itemIndex})=>{
     const dispatch = useDispatch();
 
@@ -13,15 +14,15 @@ const CartItem=({item, itemIndex})=>{
     }
 
     return(
-        <div className='flex flex-row'>
-        <div>
-            <img src={item.image} className=''></img>
+        <div className='flex flex-row justify-center gap-x-[100px] mt-[50px]'>
+        <div className='h-[180px]'>
+            <img src={item.image} height={150} width={150}></img>
         </div>
-        <div className='flex flex-col'>
-            <h1>
+        <div className='flex flex-col max-w-[500px] gap-y-5 align-baseline'>
+            <h1 className='text-xl font-bold'>
                 {item.title}
             </h1>
-            <h1>{item.description}</h1>
+            <h1 className='text-lg text-slate-800 '>{item.description}</h1>
             <div className=' flex  flex-row justify-between'>
                 <p>{item.price}</p>
                 <div onClick={removeFromCart}>
